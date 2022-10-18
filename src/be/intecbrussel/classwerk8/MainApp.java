8635beab13afce52ce34bef21626b3fe3d1cb687
alekseylee <aleksey.lee@gmail.com>
2022-10-18 11:53:24 +0200

    init commit

--- src/be/intecbrussel/classwerk8/MainApp.java
+++ src/be/intecbrussel/classwerk8/MainApp.java
@@ -5,7 +5,7 @@ import static be.intecbrussel.classwerk8.Arrays.printElements;
 public class MainApp {
     public static void main(String[] args)
     {
-        int arr[] = { 2, 3, 1, 5, 4, 9, 8, 50, 33};
+        int arr[] = { 2, 5, 1, 7 , 4, 9, 8, 50, 33};
         int n = arr.length;
         printElements(arr, n);
     }
---
+++ src/be/intecbrussel/classwerk9/ArraysOefening.java
@@ -0,0 +1,80 @@
+package be.intecbrussel.classwerk9;
+
+public class ArraysOefening {
+    public static void main(String[] args) {
+
+        //oefening 1
+
+//        int[] array1 = {1, 2, 5, 5, 8, 9, 7, 10};
+//        int[] array2 = {1, 0, 6, 15, 6, 4, 7, 0};
+//        for (int i = 0; i < array1.length; i++) {
+//            for (int j = 0; j < array2.length; j++) {
+//                if (array1[i] == array2[j]) {
+//                    System.out.println(array1[i]);
+//                }
+
+
+        //oefening 4
+
+//        String str = "Java is the best";
+//                String[] strArray = str.split(" ");
+//
+//                for (String s: strArray) {
+//                    System.out.print(s + " ");
+//                }
+
+        // oefening 3
+
+//        int[][] array = {
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//        };
+//        System.out.println(Arrays.toString(array[0]));
+//        System.out.println(Arrays.toString(array[1]));
+//        System.out.println(Arrays.toString(array[2]));
+//        System.out.println(Arrays.toString(array[3]));
+//        System.out.println(Arrays.toString(array[4]));
+//        System.out.println(Arrays.toString(array[5]));
+//        System.out.println(Arrays.toString(array[6]));
+//        System.out.println(Arrays.toString(array[7]));
+//        System.out.println(Arrays.toString(array[8]));
+
+            //oefening 3 (2nd variant)
+
+//        int[][] ar = new int[9][10];
+//        for (int[] el : ar) {
+//            for (int e : el) {
+//                System.out.print(e + " ");
+//            }
+//            System.out.println("");
+//        }
+
+        //oefening 4
+
+//        int a[][] = {{1, 3, 4}, {3, 4, 5}};
+//        int b[][] = {{1, 3, 4}, {3, 4, 5}};
+//        int[][] sum = new int[a.length][a[0].length];
+
+        // [a.length] - zameryaet vniz colonu
+        // [a[0].length - zameryaet po gorizontali colonu
+
+
+//        for (int line = 0; line < a.length; line++) {
+//            for (int column = 0; column < a[line].length; column++) {
+//                sum[line][column] = a[line][column] + b[line][column];
+//            }
+//        }
+//        System.out.println("Array 1   : " + Arrays.deepToString(a));
+//        System.out.println("Array 2   : " + Arrays.deepToString(b));
+//        System.out.println("Array sum : " + Arrays.deepToString(sum));
+
+
+    }
+}
