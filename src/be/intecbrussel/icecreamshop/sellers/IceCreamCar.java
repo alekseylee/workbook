1998a32c6b0c1c3a81c0fc175ef288906a205891
alekseylee <aleksey.lee@gmail.com>
2022-11-10 11:44:35 +0100

    init commit

--- src/be/intecbrussel/icecreamshop/sellers/IceCreamCar.java
+++ src/be/intecbrussel/icecreamshop/sellers/IceCreamCar.java
@@ -16,7 +16,8 @@ public class IceCreamCar implements IceCreamSeller {
 
     @Override
     public Cone orderCone(Cone.Flavor[] flavors) {
-        Cone cone= prepareCone(flavors);
+        Cone cone = prepareCone(flavors);
+
         if (flavors != null && flavors.length > 0) {
             profit += priceList.getBallPrice() * flavors.length;
         } else {
@@ -24,30 +25,34 @@ public class IceCreamCar implements IceCreamSeller {
         }
         return new Cone(flavors);
     }
-    private Cone prepareCone(Cone.Flavor[] flavors) {
-        if (stock.getCones() == 0 && stock.getBalls() == 0) {
-            throw new NoMoreIceCreamException();
 
-//            stock.setCones(stock.getCones() -1);
-//            stock.setBalls(stock.getBalls() - flavors.length);
-//            return new Cone();
+    private Cone prepareCone(Cone.Flavor[] flavors) {
+        if (stock.getCones() == 0 || stock.getBalls() == 0) {
+            throw new NoMoreIceCreamException();
+        }
+
+        if (stock.getBalls() >= flavors.length) {
+            stock.setCones(stock.getCones() - 1);
+            stock.setBalls(stock.getBalls() - flavors.length);
+            return new Cone();
 
         } else {
-            System.out.println("You dont have cone or balls...");
-
+            System.out.println("You have not...");
+            throw new NoMoreIceCreamException();
         }
-        return new Cone();
+
     }
 
 
     @Override
     public IceRocket orderIceRocket() {
-        IceRocket iceRocket=prepareIceRocket();
-        profit+=priceList.getRocketPrice();
+        IceRocket iceRocket = prepareIceRocket();
+        profit += priceList.getRocketPrice();
         return new IceRocket();
     }
+
     private IceRocket prepareIceRocket() {
-        if(stock.getIceRockets()==0){
+        if (stock.getIceRockets() == 0) {
             throw new NoMoreIceCreamException();
         }
         stock.setIceRockets(stock.getIceRockets() - 1);
@@ -60,11 +65,12 @@ public class IceCreamCar implements IceCreamSeller {
         profit += priceList.getMagnumPrice(type);
         return new Magnum(type);
     }
-    private Magnum prepareMagnum(Magnum.MagnumType type) throws NoMoreIceCreamException{
-        if(stock.getMagni()==0){
+
+    private Magnum prepareMagnum(Magnum.MagnumType type) throws NoMoreIceCreamException {
+        if (stock.getMagni() == 0) {
             throw new NoMoreIceCreamException();
         }
-        stock.setMagni(stock.getMagni() -1);
+        stock.setMagni(stock.getMagni() - 1);
         return new Magnum();
     }
 
@@ -72,5 +78,4 @@ public class IceCreamCar implements IceCreamSeller {
     public double getProfit() {
         return profit;
     }
-
 }
